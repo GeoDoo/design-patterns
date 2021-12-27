34787960ac95438ec6bb110436ec4b8d5b1365ad
Georgios Karametas <geometas@hotmail.com>
2021-12-27 11:16:25 +0000

    State pattern

---
+++ State/index.test.js
@@ -0,0 +1,31 @@
+const CombinationLock = require("./index");
+
+describe("state", function () {
+  it("test success", function () {
+    const cl = new CombinationLock([1, 2, 3, 4, 5]);
+
+    expect(cl.status).toEqual("LOCKED");
+    cl.enterDigit(1);
+    expect(cl.status).toEqual("1");
+    cl.enterDigit(2);
+    expect(cl.status).toEqual("12");
+    cl.enterDigit(3);
+    expect(cl.status).toEqual("123");
+    cl.enterDigit(4);
+    expect(cl.status).toEqual("1234");
+    cl.enterDigit(5);
+    expect(cl.status).toEqual("OPEN");
+  });
+
+  it("test failure", function () {
+    const cl = new CombinationLock([1, 2, 3]);
+
+    expect(cl.status).toEqual("LOCKED");
+    cl.enterDigit(1);
+    expect(cl.status).toEqual("1");
+    cl.enterDigit(2);
+    expect(cl.status).toEqual("12");
+    cl.enterDigit(5);
+    expect(cl.status).toEqual("ERROR");
+  });
+});
